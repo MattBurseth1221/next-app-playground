57ccf873be972859a88ad14f289f1d54806abf6e
Matthew Burseth <mattburseth1@gmail.com>
2024-04-12 20:54:23 -0500

    Added main page image
    Slight css changes, added background image and edited main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Main } from "next/document";
 import MainNav from "./ui/MainNav";
+import PageTitle from "./ui/PageTitle";
+import orionEarthImage from "./images/orion-and-earth.webp";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <MainNav />
-    </main>
+    <>
+      <img src={orionEarthImage.src} alt="Orion and Earth Image" className="absolute"/>
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <MainNav active={""} />
+
+        <PageTitle title="SpaceXploration"/>
+      </main>
+    </>
   );
 }
--- app/ui/PageTitle.tsx
+++ app/ui/PageTitle.tsx
@@ -5,5 +5,5 @@ type Props = {
 }
 
 export default function PageTitle({title}: Props) {
-    return <h2 className="text-4xl mb-10 border-b-[1px]">{title}</h2>;
+    return <h2 className="text-4xl mb-10 border-b-[1px] z-10">{title}</h2>;
 }
