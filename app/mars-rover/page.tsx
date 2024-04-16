fc091cef71f157025fb6711bfa16a2b508a6b37f
Matthew Burseth <mattburseth1@gmail.com>
2024-04-16 17:02:23 -0500

    Design struggles
    Hoping to find a cleaner form format for Mars Rover page - radio buttons are hell

--- app/mars-rover/page.tsx
+++ app/mars-rover/page.tsx
@@ -13,39 +13,45 @@ export default function MarsRover() {
         <p>Fill out the form below to retrieve tailored Mars rover photos:</p>
       </div>
 
-      <form className="flex flex-col text-center border-2 border-white">
-        <p>Rover Type</p>
+      <form className="flex flex-col text-center border-2 border-white w-[50%] max-w-[400px] rounded-xl">
+        <p className="mb-8 text-2xl">Rover Type</p>
 
-        <div className="flex justify-normal">
-          <label htmlFor="Opportunity" className="mb-2">
-            Opportunity
-          </label>
-          <input
-            type="radio"
-            value="Opportunity"
-            name="rover-name"
-            className="mb-4"
-          />
+        <div className="flex flex-col items-center">
+          <div className="flex justify-normal">
+            <label htmlFor="Opportunity" className="mb-2">
+              Opportunity
+            </label>
+            <input
+              type="radio"
+              value="Opportunity"
+              name="rover-name"
+              className="mb"
+            />
+          </div>
 
-          <label htmlFor="Spirit" className="mb-2">
-            Spirit
-          </label>
-          <input
-            type="radio"
-            value="Spirit"
-            name="rover-name"
-            className="mb-4"
-          />
+          <div className="flex justify-normal">
+            <label htmlFor="Spirit" className="mb-2">
+              Spirit
+            </label>
+            <input
+              type="radio"
+              value="Spirit"
+              name="rover-name"
+              className="mb-1"
+            />
+          </div>
 
-          <label htmlFor="Curiosity" className="mb-2">
-            Curiosity
-          </label>
-          <input
-            type="radio"
-            value="Curiosity"
-            name="rover-name"
-            className="mb-4"
-          />
+          <div className="flex items-center">
+            <label htmlFor="Curiosity" className="">
+              Curiosity
+            </label>
+            <input
+              type="radio"
+              value="Curiosity"
+              name="rover-name"
+              className="mb-1"
+            />
+          </div>
         </div>
       </form>
     </main>
