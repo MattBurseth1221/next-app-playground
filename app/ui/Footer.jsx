95b8e00627abcc4cb80b32a295f4ac552760d4d1
Matthew Burseth <mattburseth1@gmail.com>
2024-04-19 21:21:52 -0500

    Updated Next
    updated next.js and started adding footer element - home page photo is still overlapping the footer

---
+++ app/ui/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <footer className="bg-black w-[100%] bottom-0 h-24 flex justify-between text-white px-8 items-center">
+            <div>
+                <a href="https://mattburseth.com">Original Site</a>
+            </div>
+
+            <div>
+                Created by Matt Burseth - 2024
+            </div>
+
+            <div>
+                Space Website
+            </div>
+        </footer>
+    )
+}
